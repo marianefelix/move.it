4559b317222fba5a0a38a0f9de5ac23ab38683b6
Mariane Felix <marianefelix59@gmail.com>
2021-03-07 15:42:08 -0300

    Cria componente de barra de progresso

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import ExperienceBar from 'components/ExperienceBar';
+import styled from "styled-components";
 
-function App() {
+import './styles/global.css';
+
+const App = () => {
   return (
-    <h1>Hello NLW!</h1>
+    <Container>
+      <ExperienceBar />
+    </Container>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  /* ocupa 100% da altura da tela */
+  height: 100vh;
+  max-width: 992px;
+  
+  margin: 0 auto;
+  padding: 2.5rem 2rem;
+`;
 
 export default App;
---
+++ src/components/ExperienceBar/index.tsx
@@ -0,0 +1,28 @@
+import { 
+    Header,
+    ExperienceText,
+    Bar,
+    ProgressBar,
+    CurrentExperience
+} from 'components/ExperienceBar/style';
+
+const ExperienceBar = () => {
+    return (
+        <Header>
+           <ExperienceText>
+               0xp
+           </ExperienceText>
+           <Bar>
+                <ProgressBar />
+                <CurrentExperience>
+                    300xp
+                </CurrentExperience>
+           </Bar>
+           <ExperienceText>
+               600px
+           </ExperienceText>
+        </Header>
+    );
+};
+
+export default ExperienceBar;
