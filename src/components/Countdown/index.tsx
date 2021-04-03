bfa795c6d24322b913d240aa530e5f255d97cafd
Mariane Felix <marianefelix59@gmail.com>
2021-04-03 12:15:27 -0300

    Implementa funcionamento do crônometro

--- src/components/Countdown/index.tsx
+++ src/components/Countdown/index.tsx
@@ -1,32 +1,94 @@
-import { Fragment } from 'react';
+import { Fragment, useState, useEffect } from 'react';
 
 import {
   Container,
   TimeCard,
   Time,
   CountdownButton,
-  PlayIcon,
 } from 'components/Countdown/style';
 
-import Play from 'assets/icons/play.svg';
+import { ReactComponent as PlayIcon } from 'assets/icons/play_arrow.svg';
+import { ReactComponent as StopIcon } from 'assets/icons/stop.svg';
+import { ReactComponent as CheckIcon } from 'assets/icons/check_circle.svg';
+
+let countdownTimeout: NodeJS.Timeout;
 
 const Countdown = () => {
+  const [time, setTime] = useState(0.1 * 60);
+  const [isActive, setIsActive] = useState(false);
+  const [hasFinished, setHasFinished] = useState(false);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  /*
+    padStart: verifica se a const minutes/seconds tem 2 caracteres,
+    senão, acrescenta um zero na frente 
+    split: separa a string e retorna um array com cada caractere
+  */
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
+  const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
+
+  const startCountdown = () => {
+    setIsActive(true);
+  };
+
+  const resetCountdown = () => {
+    clearTimeout(countdownTimeout);
+    setIsActive(false);
+    setTime(0.1 * 60);
+  };
+
+  useEffect(() => {
+    if (isActive && time > 0) {
+      countdownTimeout = setTimeout(() => {
+        setTime(time - 1);
+      }, 1000);
+    } else if (isActive && time === 0) {
+      setHasFinished(true);
+      setIsActive(false);
+    }
+  }, [isActive, time]);
+
   return (
     <Fragment>
       <Container>
         <TimeCard>
-          <Time>2</Time>
-          <Time>5</Time>
+          <Time>{minuteLeft}</Time>
+          <Time>{minuteRight}</Time>
         </TimeCard>
         <Time>:</Time>
         <TimeCard>
-          <Time>0</Time>
-          <Time>0</Time>
+          <Time>{secondLeft}</Time>
+          <Time>{secondRight}</Time>
         </TimeCard>
       </Container>
-      <CountdownButton>
-        Iniciar um ciclo
-        <PlayIcon src={Play} alt="Ícone de iniciar" />
+      <CountdownButton
+        disabled={hasFinished}
+        isActive={isActive}
+        type="button"
+        onClick={isActive ? resetCountdown : startCountdown}
+      >
+        {hasFinished ? (
+          <Fragment>
+            Ciclo encerrado
+            <CheckIcon title="Ícone de finalizado" />
+          </Fragment>
+        ) : (
+          <Fragment>
+            {isActive ? (
+              <Fragment>
+                Abandonar ciclo
+                <StopIcon title="Ícone de interromper" />
+              </Fragment>
+            ) : (
+              <Fragment>
+                Iniciar um ciclo
+                <PlayIcon title="Ícone de iniciar" />
+              </Fragment>
+            )}
+          </Fragment>
+        )}
       </CountdownButton>
     </Fragment>
   );
--- src/components/Countdown/style/index.ts
+++ src/components/Countdown/style/index.ts
@@ -39,7 +39,11 @@ export const Container = styled.div`
   }
 `;
 
-export const CountdownButton = styled.button`
+interface ButtonProps {
+  isActive: boolean;
+}
+
+export const CountdownButton = styled.button<ButtonProps>`
   width: 100%;
   height: 5rem;
   margin-top: 2rem;
@@ -47,23 +51,37 @@ export const CountdownButton = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
+  text-align: center;
 
-  background: var(--blue);
-  color: var(--white);
+  background: ${(props) =>
+    props.isActive || props.disabled ? `var(--white)` : `var(--blue)`};
+  color: ${(props) =>
+    props.isActive || props.disabled ? `var(--text)` : `var(--white)`};
+  box-shadow: 0px 0px 60px rgba(0, 0, 0, 0.08);
+
+  outline: none;
   border-radius: 5px;
   border: 0;
-  outline: none;
+  border-bottom: ${(props) =>
+    props.disabled ? `4px solid var(--green)` : null};
+  cursor: ${(props) => (props.disabled ? 'unset' : 'pointer')};
 
   font-size: 1.25rem;
   font-weight: 600;
 
-  transition: background-color 0.2s;
+  transition: background-color 0.3s;
 
-  &:hover {
-    background: var(--blue-dark);
+  & svg {
+    margin-left: 10px;
   }
-`;
 
-export const PlayIcon = styled.img`
-  margin-left: 10px;
+  &:not(:disabled):hover {
+    background-color: ${(props) =>
+      props.isActive ? `var(--red)` : `var(--blue-dark)`};
+    color: var(--white);
+  }
+
+  &:hover svg path {
+    fill: ${(props) => (props.isActive ? `var(--white)` : null)};
+  }
 `;
